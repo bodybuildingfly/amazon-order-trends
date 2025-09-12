23aae339bf82e2053dc5d80912edb7ff8702fdf6
bodybuildingfly <57713654+bodybuildingfly@users.noreply.github.com>
2025-09-12 16:09:09 -0400

    Fix dev environment after folder restructure

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ set -e
 # Apply database migrations.
 # This runs the 'db-migrate' command defined in 'api/__init__.py'.
 echo "Applying database migrations..."
-export FLASK_APP=backend.api.app
+export FLASK_APP=backend.api:create_app
 flask db-migrate
 
 # Start the Gunicorn server.
 # 'exec' replaces the shell process with the Gunicorn process.
 # This is important for proper signal handling (e.g., stopping the container).
 echo "Starting Gunicorn server..."
-exec gunicorn --bind 0.0.0.0:5001 --workers 4 --worker-class gevent --timeout 120 "backend.api.app:app"
+exec gunicorn --bind 0.0.0.0:5001 --workers 4 --worker-class gevent --timeout 120 "backend.api:create_app()"
